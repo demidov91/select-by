50c1644af55e329e596ef8682f2c834fa1f9c6b6
dzmitry <demidov91@mail.ru>
2018-09-20 08:56:55 +0300

    Try 2-clusters approach.

--- exchange/static/js/config.js
+++ exchange/static/js/config.js
@@ -1,6 +1,8 @@
 ymaps.ready(initMap);
 
 var allPoints = null;
+var clusterSelected = null;
+var clusterNotSelected = null;
 
 function initMap() {
     var map = new ymaps.Map("map", {
@@ -15,23 +17,66 @@ function initMap() {
         method: "GET",
         dataType: "json"
     }).done(function(data){
-        allPoints = new ymaps.geoQuery(data.exchange.map(externalDataToPoint));
+        var barePlacemarks = data.exchange.map(externalDataToPoint);
+        
+        allPoints = new ymaps.geoQuery(barePlacemarks);
         allPoints.addToMap(map);
+
+        enableClusterization(map, barePlacemarks);
+
         configMapRelatedBehaviour(map);        
     });
-
-    
 }
 
 function configMapRelatedBehaviour(map){
     allPoints.addEvents('click', onPointClick);
-    map.events.add('click', onMapClick);
-
+    
     $(".js-clear").click(function(event){
         allPoints.each(turnPointOff);        
     });    
 
     $('.js-save').click(onSave);
+
+    map.events.add('boundschange', function(event) {
+        if (event.get('newZoom') !== event.get('oldZoom')) {
+            var stopZoom = event.get('newZoom') > 12;
+            console.log(stopZoom);
+
+            clusterSelected.options.set('clusterDisableClickZoom', stopZoom);
+            clusterNotSelected.options.set('clusterDisableClickZoom', stopZoom);        
+        }
+
+    });
+}
+
+
+function enableClusterization(map, placemarks){
+    var selected = [];
+    var notSelected = [];
+
+    for (var x of placemarks){
+        if (x.properties.get('isSelected')){
+            selected.push(x);
+        } else {
+            notSelected.push(x);
+        }
+    }
+
+    clusterSelected = new ymaps.Clusterer({
+        gridSize: 256, 
+        preset: 'islands#darkGreenClusterIcons'
+    });
+    clusterSelected.add(selected);
+
+    clusterNotSelected = new ymaps.Clusterer({
+        gridSize: 256, 
+        preset: 'islands#grayClusterIcons',
+    });
+
+    clusterNotSelected.add(notSelected);
+
+    map.geoObjects.add(clusterSelected);
+    map.geoObjects.add(clusterNotSelected);
 }
 
 function onPointClick(event){
@@ -41,17 +86,6 @@ function onPointClick(event){
     } else {
         turnPointOn(point);
     }
-}
-
-function onMapClick(event){
-    var map  = event.get('target');
-    var coordinates = event.get('coords');
-    
-    var circle = new ymaps.Circle([coordinates, 1000]);
-    map.geoObjects.add(circle);
-    var pointsToAdd = allPoints.searchInside(circle);
-    pointsToAdd.each(turnPointOn); 
-    map.geoObjects.remove(circle);   
 }
 
 function onSave(event){
@@ -75,12 +109,16 @@ function onSave(event){
 function turnPointOff(point){
     point.properties.set('isSelected', false);
     point.options.set('preset', 'islands#grayDotIcon');
+    clusterSelected.remove(point);
+    clusterNotSelected.add(point);
     return point;
 }
 
 function turnPointOn(point){
     point.properties.set('isSelected', true);
     point.options.set('preset', 'islands#darkGreenStretchyIcon');
+    clusterSelected.add(point);
+    clusterNotSelected.remove(point);
     return point;
 }
 
