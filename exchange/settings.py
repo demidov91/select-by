3277fcff14adc4c0d8b1108bd95fa3da5c5585f3
dzmitry <demidov91@mail.ru>
2018-09-28 00:57:01 +0300

    Add more environment variables usage. Remove django-compressor usage.

--- exchange/settings.py
+++ exchange/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*.dzmitry.by').split(',')
 
 
 # Application definition
@@ -48,9 +48,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.twitter',
     'allauth.socialaccount.providers.vk',
-    # I'm not sure if I need any more.
-    'compressor',
-
 ]
 
 # allauth-required.
@@ -152,7 +149,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-    "compressor.finders.CompressorFinder",
 ]
 COMPRESS_JS_FILTERS = []
 
@@ -218,8 +214,3 @@ LOGGING = {
         },
     },
 }
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
