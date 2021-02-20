4fe74e045e01e21be417e24e984130bd121ee138
Dzmitry <demidov91@mail.ru>
2021-02-20 15:54:55 +0300

    Remove mtbank loader.

--- exchange/management/commands/load_rates.py
+++ exchange/management/commands/load_rates.py
@@ -1,7 +1,5 @@
 from django.core.management.base import BaseCommand
 
-
-from exchange.mtbank import MtbankLoader
 from exchange.ideabank import IdeaBankLoader
 from exchange.selectby import SelectbyLoader
 
@@ -10,6 +8,6 @@ class Command(BaseCommand):
     help = 'Loads last rates form http://select.by'
 
     def handle(self, *args, **options):
-        for loader_class in (MtbankLoader, IdeaBankLoader, SelectbyLoader):
+        for loader_class in (IdeaBankLoader, SelectbyLoader):
             loader_class().load()
 
